08de492999c4ac5518937085c7db93315b7f8456
daffaya <daffapasya21@gmail.com>
2023-07-12 11:07:48 +0700

    Create User List Screen. Create item_user.xml as Recycler view's item.

---
+++ app/src/main/java/com/kumaa/palindrome/ui/userList/UserListActivity.kt
@@ -0,0 +1,12 @@
+package com.kumaa.palindrome.ui.userList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kumaa.palindrome.R
+
+class UserListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_list)
+    }
+}
