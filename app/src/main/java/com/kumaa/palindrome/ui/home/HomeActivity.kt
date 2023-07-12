80ff6903923d1e3d61f98f5ab1995f17233f59bd
daffaya <daffapasya21@gmail.com>
2023-07-12 11:25:05 +0700

    Create Welcoming screen

---
+++ app/src/main/java/com/kumaa/palindrome/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.kumaa.palindrome.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kumaa.palindrome.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
